58fab3f73f28721dcfec67ddc346ad57aeddeff0
Dazhen233 <118133500+Dazhen233@users.noreply.github.com>
2024-03-19 10:36:32 +0800

    Add files via upload

---
+++ serialTool/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <Qdebug>
+#include <QList>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_OpenBt_clicked();
+
+    void on_sendBt_clicked();
+
+    void readData();
+
+    void on_ClearBt_clicked();
+
+    void on_btnClearSend_clicked();
+
+private:
+
+    void InitSerialPortName();
+
+    Ui::MainWindow *ui;
+
+    QSerialPort *serial;
+};
+#endif // MAINWINDOW_H
