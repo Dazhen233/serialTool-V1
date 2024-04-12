14bac31ea69eefbf5a6585b8b75f4342c35ff097
dazhen <1012944560@qq.com>
2024-04-12 16:50:29 +0800

    改变了chart的样式，代码重新构建，图表还不能显示

---
+++ serialTool Code v0.62/mainwindow.h
@@ -0,0 +1,75 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QSizePolicy>
+#include <QtSerialPort/QSerialPortInfo>
+#include <Qdebug>
+#include <QList>
+#include <QMessageBox>
+#include <QDateTime>
+#include <QSettings>
+#include <QTimer>
+#include <QFont>
+#include<QComboBox>
+#include<QAbstractItemView>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_OpenBt_clicked();
+
+    void on_sendBt_clicked();
+
+    void readData();
+
+    void on_ClearBt_clicked();
+
+    void on_btnClearSend_clicked();
+
+    void on_chk_rev_line_stateChanged(int arg1);
+
+    void updateSerialPorts();
+
+
+    void on_autoSend_stateChanged(int arg1);
+
+    void on_sendEdit_textChanged();
+
+private:
+
+    //void updateSerialPorts();
+
+    Ui::MainWindow *ui;
+
+    QSerialPort *serial;
+
+    QString hexDataToSend;
+
+    bool firstDisplay;
+
+    void showEvent(QShowEvent *e);
+
+    void closeEvent(QCloseEvent *e);
+
+    QTimer *timer;
+
+    QTimer *autoSendTimer;
+
+    QStringList previousPorts; // 存储之前的串口列表
+};
+#endif // MAINWINDOW_H
